909975508e6506a88898e09eab396ea03b4dbc55
Gabriele Forner <gabriele.forner@icloud.com>
2026-07-24 05:05:58 +0200

    introduction of scope abstract class

--- ApiServer/Services/Schemas/ScopeRequestSchemas.cs
+++ ApiServer/Services/Schemas/ScopeRequestSchemas.cs
@@ -2,12 +2,7 @@ using MediatR;
 
 namespace Trustedbits.ApiServer.Services.Schemas;
 
-public record CreateScopeRequest : IRequest<Result<CreateScopeResponse>>
-{
-    public required string Name { get; init; }
-    public required string Value { get; init; }
-    public required string Description { get; init; }
-}
+public record CreateScopeRequest : BaseScopeSchema, IRequest<Result<CreateScopeResponse>> { }
 
 public record DescribeScopeRequest : IRequest<Result<DescribeScopeResponse>>
 {
--- ApiServer/Services/Schemas/ScopeResponseSchemas.cs
+++ ApiServer/Services/Schemas/ScopeResponseSchemas.cs
@@ -2,11 +2,17 @@ namespace Trustedbits.ApiServer.Services.Schemas;
 
 public abstract record BaseScopeSchema
 {
-    public required Guid Id { get; init; }
     public required string Name { get; init; }
     public required string Value { get; init; }
     public required string Description { get; init; }
 }
 
-public record CreateScopeResponse : BaseScopeSchema {}
-public record DescribeScopeResponse : BaseScopeSchema {}
+public record CreateScopeResponse : BaseScopeSchema
+{
+    public required Guid Id { get; init; }
+}
+
+public record DescribeScopeResponse : BaseScopeSchema
+{
+    public required Guid Id { get; init; }
+}
